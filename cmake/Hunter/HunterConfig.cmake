099e5549964364490d6d920755f73e60d4e3da0b
Kevin Scroggins <nitro404@gmail.com>
2022-04-25 00:14:49 -0400

    LibPNG Hunterization for wxWidgets

---
+++ cmake/Hunter/HunterConfig.cmake
@@ -0,0 +1,7 @@
+include_guard()
+
+hunter_config(ZLIB
+	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.12.1.zip"
+	VERSION "1.2.12.1"
+	SHA1 "b41c9126bf63dfa53fbb496e24dcceb30ddabee4"
+)
