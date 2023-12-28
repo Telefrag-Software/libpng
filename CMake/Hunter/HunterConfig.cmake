700898e057b123f166e61e463223294eee444566
Kevin Scroggins <nitro404@gmail.com>
2023-12-28 13:37:58 -0500

    Hunterize LibPNG

---
+++ CMake/Hunter/HunterConfig.cmake
@@ -0,0 +1,7 @@
+include_guard()
+
+hunter_config(ZLIB
+	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.3.0.0.zip"
+	VERSION "1.3.0.0"
+	SHA1 "553c130c05f514e5db8b4ae623347e717674a4d1"
+)
